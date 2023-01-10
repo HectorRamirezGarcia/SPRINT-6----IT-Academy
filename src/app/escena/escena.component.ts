38d4ab8250d9c1843a6df7ee0daf6ff4c5ced0c3
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-01-10 12:40:51 +0100

    Ejercicio 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
-  template:`<p>{{salida_frase}}</p>`,
   styleUrls: ['./escena.component.scss']
 })
-export class EscenaComponent implements OnInit {
+export class EscenaComponent {
   @Input() public frases = [
     " El nostre heroi estava surant l'espai sideral quan la llunyania va albirar una nau espacial", 
     "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", 
@@ -14,9 +13,24 @@ export class EscenaComponent implements OnInit {
     "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
   ];
 
-  constructor(){}
-  ngOnInit(): void {
-    
+  next() {
+    const class_frase_activa = document.getElementsByClassName("frase_activa");
+    const child_frase_activa = class_frase_activa[0];
+    let id_frase_activa = child_frase_activa.id;
+    document.getElementById(id_frase_activa+"")!.removeAttribute("class");
+    if(id_frase_activa == "3"){id_frase_activa = "-1";}
+    const next_frase = document.getElementById(""+(parseInt(id_frase_activa)+1));
+    next_frase?.setAttribute('class', 'frase_activa');
+  }
+
+  prev() {
+    const class_frase_activa = document.getElementsByClassName("frase_activa");
+    const child_frase_activa = class_frase_activa[0];
+    let id_frase_activa = child_frase_activa.id;
+    document.getElementById(id_frase_activa+"")!.removeAttribute("class");
+    if(id_frase_activa == "0"){id_frase_activa = "4";}
+    const next_frase = document.getElementById(""+(parseInt(id_frase_activa)-1));
+    next_frase?.setAttribute('class', 'frase_activa');
   }
 
 }
