701eea197ffc0d3e9495067583879ac65f7425a2
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-01-10 10:12:50 +0100

    Ejercicio 2

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
+  template:`<p>{{salida_frase}}</p>`,
   styleUrls: ['./escena.component.scss']
 })
-export class EscenaComponent {
+export class EscenaComponent implements OnInit {
+  @Input() public frases = [
+    " El nostre heroi estava surant l'espai sideral quan la llunyania va albirar una nau espacial", 
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", 
+    "L'heroi va decidir travessar la porta que el portava a casa", 
+    "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+  ];
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 
 }
