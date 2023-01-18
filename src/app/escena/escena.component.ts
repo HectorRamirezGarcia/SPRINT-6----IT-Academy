cb1f0db20f8afbe44454a9eacd489bee2fc30e7d
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-01-18 15:27:30 +0100

    Refactor code

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,45 +1,25 @@
-import { Component, Input, Renderer2 } from '@angular/core';
+import { HomeComponent } from './../home/home.component';
+import { Component, Input, Renderer2, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
   styleUrls: ['./escena.component.scss']
 })
-export class EscenaComponent {
-  @Input() public frases = [
-    { frase: "El nostre heroi estava surant l'espai sideral quan la llunyania va albirar una nau espacial.", img: "../../assets/img/1.jpg" },
-    { frase: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img: "../../assets/img/2.jpg"},
-    { frase: "L'heroi va decidir travessar la porta que el portava a casa", img: "../../assets/img/3.jpg" },
-    { frase: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...", img: "../../assets/img/4.jpg" },
-  ];
-background: any;
-
+export class EscenaComponent extends HomeComponent {
+  contador = 0;
   next() {
-    
-    const class_frase_activa = document.getElementsByClassName("frase_activa");
-    const child_frase_activa = class_frase_activa[0];
-    let id_frase_activa = child_frase_activa.id;
-    document.getElementById(id_frase_activa+"")!.removeAttribute("class");
-    if(id_frase_activa == (""+(this.frases.length-1))){id_frase_activa = "-1";}
-    const next_frase = document.getElementById(""+(parseInt(id_frase_activa)+1));
-    next_frase?.setAttribute('class', 'frase_activa');
-
-    const body = (document.getElementById("body-escena") as HTMLElement);
-    body!.style.backgroundImage = "url("+this.frases[(parseInt(id_frase_activa)+1)].img+")";
-    
+    document.getElementById(""+this.contador)!.classList.remove("active");
+    this.contador++
+    if(this.contador == this.frases.length){this.contador = 0;}
+    document.getElementById(""+this.contador)!.className = "active";
   }
 
   prev() {
-    const class_frase_activa = document.getElementsByClassName("frase_activa");
-    const child_frase_activa = class_frase_activa[0];
-    let id_frase_activa = child_frase_activa.id;
-    document.getElementById(id_frase_activa+"")!.removeAttribute("class");
-    if(id_frase_activa == "0"){id_frase_activa = String(this.frases.length)}
-    const next_frase = document.getElementById(""+(parseInt(id_frase_activa)-1));
-    next_frase?.setAttribute('class', 'frase_activa');
-
-    const body = document.getElementById("body-escena");
-    body!.style.backgroundImage = "url("+this.frases[(parseInt(id_frase_activa)+1)].img+")";
+    document.getElementById(""+this.contador)!.classList.remove("active");
+    this.contador--
+    if(this.contador == -1){this.contador = this.frases.length-1;}
+    document.getElementById(""+this.contador)!.className = "active";
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,10 @@ import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/co
 })
 export class HomeComponent {
   join = true;
+  @Input() public frases = [
+    { frase: "El nostre heroi estava surant l'espai sideral quan la llunyania va albirar una nau espacial.", img: "../../assets/img/1.jpg" },
+    { frase: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img: "../../assets/img/2.jpg"},
+    { frase: "L'heroi va decidir travessar la porta que el portava a casa", img: "../../assets/img/3.jpg" },
+    { frase: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...", img: "../../assets/img/4.jpg" },
+  ];
 }
