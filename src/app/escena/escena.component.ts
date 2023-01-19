e220a6dcb5c17484e23ce102962c0980097c36b7
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-01-19 11:42:07 +0100

    Refactor Code final

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,5 +1,5 @@
 import { HomeComponent } from './../home/home.component';
-import { Component, ElementRef} from '@angular/core';
+import { Component, ElementRef, Input} from '@angular/core';
 
 @Component({
   selector: 'app-escena',
@@ -10,19 +10,13 @@ export class EscenaComponent extends HomeComponent {
   contador = 0;
   bg_img = "url('../../assets/img/"+(this.contador+1)+".jpg')";
   next() {
-    document.getElementById(""+this.contador)!.classList.remove("active");
-    this.contador++
+    this.contador++;
     if(this.contador == this.frases.length){this.contador = 0;}
     this.bg_img = "url('../../assets/img/"+(this.contador+1)+".jpg')";
-    document.getElementById(""+this.contador)!.className = "active";
   }
-
   prev() {
-    document.getElementById(""+this.contador)!.classList.remove("active");
-    this.contador--
+    this.contador--;
     if(this.contador == -1){this.contador = this.frases.length-1;}
     this.bg_img = "url('../../assets/img/"+(this.contador+1)+".jpg')";
-    document.getElementById(""+this.contador)!.className = "active";
   }
-
 }
